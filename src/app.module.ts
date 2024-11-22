92b8b8b0883e42037d0c2d89a9ec204929dfae7e
Joshua Morin <73865022+TheRevBv@users.noreply.github.com>
2024-11-22 16:40:34 -0600

    falta config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+
 import { PedidoModule } from './pedido/pedido.module';
 import { PizzaModule } from './pizza/pizza.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     PedidoModule,
     PizzaModule,
